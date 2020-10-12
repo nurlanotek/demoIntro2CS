89f1131b4cce8506f4565dd59ecc44a4350da8e9
Nurlan Shaidullaev <nurlanotek@gmail.com>
2020-10-12 15:35:49 +0600

    Print the third range of numbers

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     printRangeOfNumbers(3, 10);
     printRangeOfNumbersBackWards(3, 10);
+    printRangeOfNumbers(50, 10);
   }
 
   public static void printRangeOfNumbers(int start, int end){
