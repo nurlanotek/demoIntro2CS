b6ada85bcd1d5d19bb6db48904018861784a97f5
Nurlan Shaidullaev <nurlanotek@gmail.com>
2020-10-12 15:31:54 +0600

    Add new method to print numbers in Reverse order

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,19 @@ public class HelloWorld {
     System.out.println("Hello world!");
 
     printRangeOfNumbers(3, 10);
+    printRangeOfNumbersBackWards(3, 10);
   }
 
   public static void printRangeOfNumbers(int start, int end){
     for(int i = start; i <= end; i++) {
+      System.out.printf("%d ", i);
+    }
+
+    System.out.println();
+  }
+
+  public static void printRangeOfNumbersBackWards(int start, int end){
+    for(int i = end; i >= start; i--) {
       System.out.printf("%d ", i);
     }
 
