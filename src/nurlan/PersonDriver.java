08a8e7d0ba014906df54080283c959af4f229356
Nurlan Shaidullaev <nurlan.shaidullaev@ucentralasia.org>
2020-11-16 17:15:00 +0600

    Updated PersonDriver.java with some new output statements

--- src/nurlan/PersonDriver.java
+++ src/nurlan/PersonDriver.java
@@ -4,5 +4,8 @@ public class PersonDriver {
     public static void main(String[] args) {
         Person person = new Person("nurlan", "nurlan@gmail.kg", "qwerty");
         System.out.println(person);
+        System.out.println(person.getFirstName());
+        System.out.println(person.getEmail());
+        System.out.println(person.getPassword());
     }
 }
